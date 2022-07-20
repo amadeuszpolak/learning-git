4fcc9d2fc2ccac9a2e349f47a6b1af68330081d5
amadeuszpolak <amadeusz.polak@gmail.com>
2022-07-20 12:16:39 +0200

    Update main.py
    Fix Sketch Info

--- main.py
+++ main.py
@@ -1,12 +1,1 @@
-numbers_list = []
-numbers_div_5_list = []
-numbers_div_5_square_3_list = []
-for i in range(0,101):
-    numbers_list.append(i)
-for number in numbers_list:
-    if number%5 == 0:
-        numbers_div_5_list.append(number)
-for number in numbers_div_5_list:
-    numbers_div_5_square_3_list.append(number**3)
-print(numbers_div_5_list)
-print(numbers_div_5_square_3_list)
+print("Martwa papuga to najlepszy skecz grupy Monthy Pythona")
